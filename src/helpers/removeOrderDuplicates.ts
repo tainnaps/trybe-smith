88864282dda020b91b173e39439b1668da907511
Taina Santana <tainnaps@gmail.com>
2022-05-15 20:04:56 -0300

    feat: add removeOrderDuplicates helper

---
+++ src/helpers/removeOrderDuplicates.ts
@@ -0,0 +1,20 @@
+import { IOrder } from '../interfaces/order';
+
+/*
+  | REFERENCE |
+  Usei o link abaixo como referência para remover itens repetidos num array de objetos.
+  link: https://dev.to/marinamosti/removing-duplicates-in-an-array-of-objects-in-js-with-sets-3fep
+*/
+
+const removeOrderDuplicates = (orders: IOrder[]): IOrder[] => {
+  const ordersIds = Array.from(new Set(orders.map(({ id }) => id)));
+
+  const uniqueOrders = ordersIds.map((orderId) => {
+    const index = orders.findIndex(({ id }) => id === orderId);
+    return orders[index];
+  });
+
+  return uniqueOrders;
+};
+
+export default removeOrderDuplicates;
