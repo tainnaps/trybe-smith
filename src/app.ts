0feab01ddd1295087bf7ce8a8f400810eda7d9ab
Taina Santana <tainnaps@gmail.com>
2022-05-15 01:10:55 -0300

    feat: add error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import errorMiddleware from './middlewares/error';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errorMiddleware);
 
 export default app;
