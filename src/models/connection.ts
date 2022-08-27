2e97157729b82d8a067abbf6edeafebcbd45ba64
Tainá Santana <tainnaps@gmail.com>
2022-08-27 00:28:06 -0300

    refactor: get mysql port from env file

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,12 +1,11 @@
-import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
-
-dotenv.config();
+import 'dotenv/config';
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  port: Number(process.env.MYSQL_PORT) || 3306,
 });
 
 export default connection;
